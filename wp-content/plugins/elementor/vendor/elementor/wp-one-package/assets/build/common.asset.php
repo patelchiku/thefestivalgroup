7d79b4add706f6f63812fe8fc5cdea9b49817da8
patelchiku <patelchiku@gmail.com>
2026-08-25 02:21:08 +0530

    Remove Elementor - unused, page already rendered via WPBakery

--- wp-content/plugins/elementor/vendor/elementor/wp-one-package/assets/build/common.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1f031a48279dd8a7d4bd');
--- wp-content/plugins/elementor/vendor/elementor/wp-one-package/assets/build/manage-dashboard-widget.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fbf4651d9df10ea54d4b');
